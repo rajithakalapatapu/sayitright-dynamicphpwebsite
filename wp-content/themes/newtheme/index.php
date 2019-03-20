8914d056f0ea7c29e62c941afd8703b1744ba05e
rajitha <rajithakalapatapu9@gmail.com>
2019-03-19 22:18:16 -0500

    initial commit
    basic wordpress theme
    all of code from project 2

---
+++ wp-content/themes/newtheme/index.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="sayitright.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Rajdhani' rel='stylesheet'>
+</head>
+
+<body>
+    <nav>
+        <div class="nav_left">
+            <a href="HomePage.html">
+                <img src="imgsay/logo.png">
+            </a>
+        </div>
+        <div class="nav_right">
+            <ul>
+                <li><a href="HomePage.html">Home</a></li>
+                <li><a href="AboutUs.html">About US</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="buyfromus.html">Purchase</a></li>
+                <li><a href="contactus.html" class="activetab">Contact US</a></li>
+                <li><a href="signup.html">Sign Up</a></li>
+                <li><a href="login.html">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="copyright">
+        <p> <br> </p>
+        <p> <br> </p>
+        <p> <br> </p>
+        <p class="white"> Copyright &copy 2019 All rights reserved</p>
+        <p class="white"> | This web is made with &#9825;</p>
+        <p class="blue">by DiazApps </p>
+    </div>
+
+
+</body>
+</html>
---
+++ wp-content/themes/rajitha/header.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="sayitright.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Rajdhani' rel='stylesheet'>
+</head>
+
+<body>
+    <nav>
+        <div class="nav_left">
+            <a href="HomePage.html">
+                <img src="imgsay/logo.png">
+            </a>
+        </div>
+        <div class="nav_right">
+            <ul>
+                <li><a href="HomePage.html">Home</a></li>
+                <li><a href="AboutUs.html">About US</a></li>
+                <li><a href="index.php" class="activetab">Blog</a></li>
+                <li><a href="buyfromus.html">Buy From US</a></li>
+                <li><a href="contactus.html">Contact US</a></li>
+                <li><a href="signup.html">Sign Up</a></li>
+                <li><a href="login.html">Login</a></li>
+            </ul>
+        </div>
+    </nav>
