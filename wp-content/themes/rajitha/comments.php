bf494c46949280415da09d26c32615b39245aa2d
rajitha <rajithakalapatapu9@gmail.com>
2019-03-23 11:56:56 -0500

    custom comment format

--- wp-content/themes/rajitha/comments.php
+++ wp-content/themes/rajitha/comments.php
@@ -3,12 +3,70 @@
 	<?php
 
 	function format_comment($comment, $args, $depth) {
+
+		if ( 'div' === $args['style'] ) {
+			$tag       = 'div';
+			$add_below = 'comment';
+		} else {
+			$tag       = 'li';
+			$add_below = 'div-comment';
+		}
+	?>
+
+		<<?php echo $tag; ?>
+		<?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?>
+		id="comment_box comment-<?php comment_ID() ?>"><?php
+		if ( 'div' != $args['style'] ) { ?>
+			<div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
+		} ?>
+		<div class="comment-author vcard"><?php
+		printf( __( '<cite class="fn">%s</cite>' ), get_comment_author_link() );
+		?>
+		</div><?php
+		if ( $comment->comment_approved == '0' ) { ?>
+			<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php
+		} ?>
+		<div class="comment-meta commentmetadata">
+			<?php
+				/* translators: 1: date, 2: time */
+				printf(
+					__('%1$s at %2$s'),
+					get_comment_date(),
+					get_comment_time()
+				);
+			?>
+			<?php
+				edit_comment_link( __( '(Edit)' ), '  ', '' );
+			?>
+		</div>
+
+		<div class="comment-reply"><?php
+		comment_reply_link(
+			array_merge(
+				$args,
+				array(
+					'add_below' => $add_below,
+					'depth'     => $depth,
+					'max_depth' => $args['max_depth']
+				)
+			)
+		); ?>
+		</div>
+
+		<div class="comment-text">
+			<?php comment_text(); ?>
+		</div>
+
+		<?php
+		if ( 'div' != $args['style'] ) : ?>
+			</div><?php
+		endif;
 	}
 
 	wp_list_comments(
 		array(
-		'style' => 'div',
-		'callback' => format_comment
+			'style' => 'div',
+			'callback' => format_comment
 		)
 	);
 	?>
