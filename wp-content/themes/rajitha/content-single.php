acbc00eca297bc5a51b62dc1f495b1e68d32406e
rajitha <rajithakalapatapu9@gmail.com>
2019-03-23 10:20:48 -0500

    format navigation and comments count

--- wp-content/themes/rajitha/content-single.php
+++ wp-content/themes/rajitha/content-single.php
@@ -24,41 +24,54 @@
 			<?php the_content(); ?>
 		</div>
 	</div>
-</div>
 
-<div class="navigation">
-	<div class="prev_post">
-		<?php
-		$previous_post = get_previous_post();
-		if ( is_a( $previous_post , 'WP_Post' ) ) : ?>
-			<a href="<?php echo get_permalink( $previous_post->ID ); ?>"><?php echo get_the_title( $previous_post->ID ); ?></a>
-		<?php endif; ?>
+	<div class="post_navigation">
+		<div class="prev_post">
+			<?php
+			$previous_post = get_previous_post();
+			if ( is_a( $previous_post , 'WP_Post' ) ) : ?>
+				<div class="post_navigation_direction">
+					Prev Post
+				</div>
+				<a class="post_navigation_link" href="<?php echo get_permalink( $previous_post->ID ); ?>">
+					<?php echo get_the_title( $previous_post->ID ); ?>
+				</a>
+			<?php endif; ?>
+		</div>
+		<div class="next_post">
+			<?php
+			$next_post = get_next_post();
+			if ( is_a( $next_post , 'WP_Post' ) ) : ?>
+				<div class="post_navigation_direction">
+					Next Post
+				</div>
+				<a  class="post_navigation_link" href="<?php echo get_permalink( $next_post->ID ); ?>">
+					<?php echo get_the_title( $next_post->ID ); ?>
+				</a>
+			<?php endif; ?>
+		</div>
 	</div>
-	<div class="next_post">
-		<?php
-		$next_post = get_next_post();
-		if ( is_a( $next_post , 'WP_Post' ) ) : ?>
-			<a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo get_the_title( $next_post->ID ); ?></a>
-		<?php endif; ?>
-	</div>
-</div>
 
-<div class="comment list">
-	<span class="post-comments">
-		<?php comments_popup_link('No comments', '1 Comment', '% Comments'); ?>
-	</span>
-	<?php comments_template(); ?>
-</div>
-<?php
-$comments_args = array(
+
+	<div class="comments">
+		<span class="comments_count">
+			<?php comments_number('No comments', '1 Comment', '% Comments'); ?>
+		</span>
+		<?php comments_template(); ?>
+	</div>
+	<?php
+	$comments_args = array(
         // Change the title of send button
-	'label_submit' => __( 'Send', 'textdomain' ),
+		'label_submit' => __( 'Send', 'textdomain' ),
         // Change the title of the reply section
-	'title_reply' => __( 'Write a Reply or Comment', 'textdomain' ),
+		'title_reply' => __( 'Write a Reply or Comment', 'textdomain' ),
         // Remove "Text or HTML to be displayed after the set of comment fields".
-	'comment_notes_after' => '',
+		'comment_notes_after' => '',
         // Redefine your own textarea (the comment body).
-	'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
-);
-comment_form( $comments_args );
-?>
+		'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+	);
+	comment_form( $comments_args );
+	?>
+
+</div>
+
