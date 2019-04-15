bdd4b3c62613e9bf38b58e35c66edb8ec23ed626
rajitha <rajithakalapatapu9@gmail.com>
2019-04-14 19:50:12 -0500

    buy from us shipping html validation

--- buyfromus2.php
+++ buyfromus2.php
@@ -35,37 +35,46 @@ $_SESSION['total'] = 0;
     <div class="content" id="wrapper">
         <p class="buyfromuscontenttitle"> Buy From Us </p>
         <div class="shipping">
-            <div class="shipping_left">
-                <h2 class="shippingh3"> Contact Information </h2>
-                <input name="email" placeholder="Enter Email" type="text">
-                <h2 class="shippingh3"> Shipping address </h2>
-                <div class="shipping_one_line">
-                    <input type="text" name="fname" value="Enter First name">
-                    <input type="text" name="lname" value="Enter Last name">
-                </div>
-                <input type="text" name="lname" value="Enter Address">
-                <input type="text" name="lname" value="Enter Apartment, suite, etc.">
-                <input type="text" name="lname" value="Enter City">
-                <div class="shipping_one_line">
-                    <select>
-                        <option value="English">English</option>
-                        <option value="Spanish">Spanish</option>
-                    </select>
-                    <input type="text" name="lname" value="Enter Postal Code">
-                </div>
-                <button class="shippingsend" id="button">PLACE ORDER</button>
-                <button class="shippingsend" id="button" onclick="clear_cart()">CLEAR CART</button>
-            </div>
-            <div class="shipping_right">
-                <div class="shipping_right">
-                    <div class="shipping_table">
-                        <?php
-                        require_once('dboperations.php');
-                        function get_each_row($product_id, $quantity) {
-                            $product_details = get_product_details($product_id);
 
-                            $details = $product_details->fetch();
-                            $each_cart_item = "
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER[PHP_SELF]); ?>">
+                <div class="shipping_left">
+                    <h2 class="shippingh3"> Contact Information </h2>
+                    <input name="email" id="email" placeholder="Enter Email" type="text" required>
+                    <span id="emailErr" class="error"></span>
+                    <h2 class="shippingh3"> Shipping address </h2>
+                    <div class="shipping_one_line">
+                        <input type="text" id="fname" name="fname" placeholder="Enter First name" required>
+                        <span id="fnameErr" class="error"></span>
+                        <input type="text" id="lname" name="lname" placeholder="Enter Last name" required>
+                        <span id="lnameErr" class="error"></span>
+                    </div>
+                    <input type="text" name="address" id="address" placeholder="Enter Address" required>
+                    <span id="addressErr" class="error"></span>
+                    <input type="text" name="apartment" id="apartment" placeholder="Enter Apartment, suite, etc." required>
+                    <span id="apartmentErr" class="error"></span>
+                    <input type="text" name="city" id="city" placeholder="Enter City" required>
+                    <span id="cityErr" class="error"></span>
+                    <div class="shipping_one_line">
+                        <select>
+                            <option value="English">English</option>
+                            <option value="Spanish">Spanish</option>
+                        </select>
+                        <input type="text" name="postal" id="postal" placeholder="Enter Postal Code" required>
+                        <span id="postalErr" class="error"></span>
+                    </div>
+                    <button class="shippingsend" id="button">PLACE ORDER</button>
+                    <button class="shippingsend" id="button" onclick="clear_cart()">CLEAR CART</button>
+                </div>
+                <div class="shipping_right">
+                    <div class="shipping_right">
+                        <div class="shipping_table">
+                            <?php
+                            require_once('dboperations.php');
+                            function get_each_row($product_id, $quantity) {
+                                $product_details = get_product_details($product_id);
+
+                                $details = $product_details->fetch();
+                                $each_cart_item = "
                             <div class=\"shipping_table_data\">
                                 <img src=\"%s\">
                             </div>
@@ -80,15 +89,15 @@ $_SESSION['total'] = 0;
                             </div>
                             ";
 
-                            $_SESSION['total'] += $details['price_per_unit'] * $quantity;
-                            return sprintf($each_cart_item, $details['product_picture'], $details['product_name'], $quantity,
-                                $details['price_per_unit'] * $quantity);
-                        }
+                                $_SESSION['total'] += $details['price_per_unit'] * $quantity;
+                                return sprintf($each_cart_item, $details['product_picture'], $details['product_name'], $quantity,
+                                    $details['price_per_unit'] * $quantity);
+                            }
 
-                        $cart_items = $_SESSION['cart'];
-                        if(!empty($cart_items)) {
+                            $cart_items = $_SESSION['cart'];
+                            if(!empty($cart_items)) {
 
-                            echo "
+                                echo "
                                 
                             <div class=\"shipping_table_data\">
                                 <h5> ID </h5>
@@ -105,11 +114,11 @@ $_SESSION['total'] = 0;
                             ";
 
 
-                            foreach($cart_items as $product_id => $quantity) {
-                                echo get_each_row($product_id, $quantity['quantity']);
-                            }
+                                foreach($cart_items as $product_id => $quantity) {
+                                    echo get_each_row($product_id, $quantity['quantity']);
+                                }
 
-                            $total_line = "
+                                $total_line = "
                             <div class=\"shipping_table_data\">
                                 <p> Total </p>
                             </div>
@@ -123,17 +132,20 @@ $_SESSION['total'] = 0;
                                 <p> %s </p>
                             </div>";
 
-                            echo sprintf($total_line, $_SESSION['total']);
-                        } else {
-                            echo "Cart is empty";
-                        }
+                                echo sprintf($total_line, $_SESSION['total']);
+                            } else {
+                                echo "Cart is empty";
+                            }
 
 
-                        ?>
+                            ?>
 
+                        </div>
                     </div>
                 </div>
-            </div>
+
+            </form>
+
         </div>
     </div>
     <div class="copyright">
